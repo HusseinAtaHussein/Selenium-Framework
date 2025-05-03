4c7bdfbce8a05aee1d9c7df01772ad557773386a
Hussein Ata <Hussein.Ata@cis.asu.edu.eg>
2025-05-03 15:23:06 +0300

    To Test Jenkins

--- src/test/java/Tests/UserRegisterationTest.java
+++ src/test/java/Tests/UserRegisterationTest.java
@@ -26,7 +26,7 @@ public class UserRegisterationTest extends TestBase{
     public LoginPage LoginPageObject;
     @Test(priority = 1)
     public void UserRegisterationSucessfully(){
-        homePageObject = new HomePage(driver);
+ /*       homePageObject = new HomePage(driver);
         homePageObject.OpenRegisterationLink();
 
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
@@ -40,7 +40,8 @@ public class UserRegisterationTest extends TestBase{
                 "Hossam111@gmail.com",
                 "Password@123456",
                 "Password@123456"
-        );
+        );*/
+        System.out.println("Yes");
     }
     /*@Test(priority = 2,dependsOnMethods = "UserRegisterationSucessfully")
     public void RegisterUserCanLogout(){
